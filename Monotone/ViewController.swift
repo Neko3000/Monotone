113931d714115bd05e98cd798d50d7aeba155e27
Neko3000 <Neko3000@live.com>
2020-10-29 09:57:44 +0800

    init(Project): initiazlize project structure

---
+++ Monotone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Monotone
+//
+//  Created by Xueliang Chen on 2020/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
