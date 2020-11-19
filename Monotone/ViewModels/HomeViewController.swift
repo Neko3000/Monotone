d43a8b25d6397b6b3e59462daa6932d5be5b0ae1
Neko3000 <Neko3000@live.com>
2020-11-19 09:29:46 +0800

    feat(HomeViewModel): add HomeViewModel

---
+++ Monotone/ViewModels/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  Monotone
+//
+//  Created by Xueliang Chen on 2020/11/19.
+//
+
+import Foundation
+
+import RxSwoft
+import RxRelay
+import Action
+
+class HomeViewController: BaeViewController{
+    
+    // MARK: Input
+//    struct Input {
+//        var searchQuery: Beha
+//    }
+}
