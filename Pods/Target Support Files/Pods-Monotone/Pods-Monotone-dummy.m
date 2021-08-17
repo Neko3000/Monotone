1b41cd500decdc7ba1e41c00616164056c0d7c19
Neko3000 <neko3000@live.com>
2021-08-17 14:00:43 +0800

    feat(Pods): remove pods

--- Pods/Target Support Files/Pods-Monotone/Pods-Monotone-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Monotone : NSObject
-@end
-@implementation PodsDummy_Pods_Monotone
-@end
--- Pods/Target Support Files/anim/anim-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_anim : NSObject
-@end
-@implementation PodsDummy_anim
-@end
